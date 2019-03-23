775af5a69008055cfec104cf35c188a10f72b9b1
viniciusofp <viniciusofp@gmail.com>
2019-03-23 12:36:40 -0300

    Updates

---
+++ precache-manifest.82fb716f6bb4da968b5a36b2f1d301c1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fb2e7c85679f29ceecd",
+    "url": "/angloamericana-react/static/css/main.5358a5bd.chunk.css"
+  },
+  {
+    "revision": "8fb2e7c85679f29ceecd",
+    "url": "/angloamericana-react/static/js/main.30dbf453.chunk.js"
+  },
+  {
+    "revision": "8e7ef95f8fe4865da0d6",
+    "url": "/angloamericana-react/static/js/runtime~main.dad22cdd.js"
+  },
+  {
+    "revision": "d0fc00d94e44e7919e91",
+    "url": "/angloamericana-react/static/js/2.03716bb7.chunk.js"
+  },
+  {
+    "revision": "8255331106e8d7f3a9c7ee6d6aa1d8ca",
+    "url": "/angloamericana-react/static/media/reforma.82553311.jpg"
+  },
+  {
+    "revision": "de93041ccb6f7953c356e23f15ab95ff",
+    "url": "/angloamericana-react/static/media/cobertura.de93041c.jpg"
+  },
+  {
+    "revision": "04b31e68ac10de3e2a315cce0b1d54df",
+    "url": "/angloamericana-react/static/media/mobiliado.04b31e68.jpg"
+  },
+  {
+    "revision": "4d71c614e845ed04f3734c0e65580857",
+    "url": "/angloamericana-react/static/media/ibirapuera.4d71c614.jpg"
+  },
+  {
+    "revision": "bfaba7b8c37953fceafc0ba7e74792e0",
+    "url": "/angloamericana-react/static/media/condominioclube.bfaba7b8.jpeg"
+  },
+  {
+    "revision": "0cb5945e6a0921dba4859b2c1de8d72e",
+    "url": "/angloamericana-react/index.html"
+  }
+];
